37ea495a97d7c9feebb1ad9d5efb92b0a21b36fd
ivan balandra camacho <balandracamachoivan@gmail.com>
2025-12-11 20:33:47 -0500

    Migración a front-v2 con Docker y arreglo de AntDesign

--- front-v2/lib/AntdRegistry.tsx
+++ front-v2/lib/AntdRegistry.tsx
@@ -1,32 +1,8 @@
 'use client';
 
 import React from 'react';
-import { createCache, extractStyle, StyleProvider } from '@ant-design/cssinjs';
-import { useServerInsertedHTML } from 'next/navigation';
+import { AntdRegistry } from '@ant-design/nextjs-registry';
 
 export function AntdStyleRegistry({ children }: { children: React.ReactNode }) {
-  const [cache] = React.useState(() => createCache());
-  const isServerInserted = React.useRef<boolean>(false);
-
-  useServerInsertedHTML(() => {
-    // Avoid duplicate CSS insert
-    if (isServerInserted.current) {
-      return;
-    }
-    isServerInserted.current = true;
-    
-    const styleText = extractStyle(cache, true);
-    return (
-      <style
-        id="antd"
-        dangerouslySetInnerHTML={{ __html: styleText }}
-      />
-    );
-  });
-
-  return (
-    <StyleProvider cache={cache} hashPriority="high" ssrInline>
-      {children}
-    </StyleProvider>
-  );
+  return <AntdRegistry>{children}</AntdRegistry>;
 }
